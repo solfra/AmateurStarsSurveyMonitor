728091933a385209937c14097bafb110a7db3797
Frank Soldano <f.soldano38@orange.fr>
2024-12-16 20:19:14 +0100

    Add preview with matplot

--- python/ReadFits.py
+++ python/ReadFits.py
@@ -9,6 +9,8 @@
 
 from astropy.io import fits
 import argparse
+import matplotlib.pyplot as plt
+from astropy.visualization import ZScaleInterval, ImageNormalize
 
 parser = argparse.ArgumentParser(description='Read a Fits file')
 parser.add_argument('file',type=str, help='File to read')
@@ -19,3 +21,9 @@ file = args.file
 fitsFile = fits.open(file)
 for key in fitsFile[0].header:
     print(f' {key}: {fitsFile[0].header[key]}')
+
+img_data = fitsFile[0].data
+norm = ImageNormalize(img_data, interval=ZScaleInterval())
+plt.figure(figsize=(15,15))
+plt.imshow(img_data,norm=norm,origin='lower',cmap='gray')
+plt.show()
