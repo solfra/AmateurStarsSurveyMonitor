41b22119f32271331602d7c286d93e0855f200dc
Frank Soldano <f.soldano38@orange.fr>
2024-12-27 12:42:37 +0100

    Add gaia sources in the image plot

--- python/ReadFits.py
+++ python/ReadFits.py
@@ -14,6 +14,8 @@ from astropy.visualization import ZScaleInterval, ImageNormalize
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 from astroquery.gaia import Gaia
+from astropy.wcs import WCS
+from tqdm import trange
 
 parser = argparse.ArgumentParser(description='Read a Fits file')
 parser.add_argument('file',type=str, help='File to read')
@@ -28,15 +30,30 @@ img_header = fitsFile[0].header
 for key in img_header:
     print(f' {key}: {img_header[key]}')
 
-norm = ImageNormalize(img_data, interval=ZScaleInterval())
-plt.figure(figsize=(15,15))
-plt.imshow(img_data,norm=norm,origin='lower',cmap='gray')
-plt.show()
-
 coord = SkyCoord(img_header['CRVAL1']*u.deg,img_header['CRVAL2']*u.deg)
 
-coneGaia = u.Quantity(5, u.deg)
+coneGaia = u.Quantity(3, u.deg)
 
 print('Query gaia stars...')
 Gaia.ROW_LIMIT = -1
 Gaia.cone_search_async(coordinate=coord, radius=coneGaia, output_file=f'{file}_gaia_cone.fits.gz',output_format='fits',dump_to_file=True)
+
+wcs = WCS(img_header) 
+
+gaiaTable = fits.open(f'{file}_gaia_cone.fits.gz')[1].data
+
+x_gaia, y_gaia = [], []
+
+for i in trange(len(gaiaTable['source_id'])): 
+    stars = wcs.world_to_pixel(SkyCoord(gaiaTable['ra'][i]*u.deg,gaiaTable['dec'][i]*u.deg))
+
+    if 0<stars[0]<img_data.shape[0] and 0<stars[1]<img_data.shape[1] and gaiaTable['phot_g_mean_mag'][i]<16:
+        x_gaia.append(stars[0])
+        y_gaia.append(stars[1])
+print('Number of stars found in img:',len(x_gaia))
+
+norm = ImageNormalize(img_data, interval=ZScaleInterval())
+plt.figure(figsize=(15,15))
+plt.imshow(img_data,norm=norm,origin='lower',cmap='gray')
+plt.plot(x_gaia,y_gaia,'r.')
+plt.show()
