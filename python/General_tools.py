bc56ba0360c2ca0db3f5f82c2b1a3084b29cc32d
Frank Soldano <f.soldano38@orange.fr>
2025-04-29 10:18:27 +0200

    Add position array

--- python/General_tools.py
+++ python/General_tools.py
@@ -5,3 +5,9 @@ def init_dict_stat(stat_name):
     for name in stat_name :
         stat[name] = np.array([])
     return stat
+
+def create_position_array(x,y):
+    pos = []
+    for i in range(len(x)):
+        pos.append( [x[i] , y[i]] ) 
+    return pos
